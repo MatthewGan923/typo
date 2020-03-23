094319ca33b907efd23a9fbf009377891bb17cea
EC2 Default User <ec2-user@ip-172-31-81-95.ec2.internal>
2020-03-23 21:46:01 +0000

    finish everything

--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -11,6 +11,27 @@ describe Admin::CategoriesController do
     request.session = { :user => henri.id }
   end
 
+  describe "test_create" do
+    before(:each) do
+      get :edit, :id => Factory(:category).id
+    end
+
+    it 'should render template new' do
+      assert_template 'new'
+      assert_tag :tag => "table",
+        :attributes => { :id => "category_container" }
+    end
+
+    it 'should create a new category' do
+      post :edit, :category => {:name => "Foobar", :keywords => "Lorem Ipsam", :permalink => "GG", :description => "Descrip"}
+      assert_response :redirect, :action => "index"
+      assigns(:categories).should_not be_nil
+      expect(flash[:notice]).to eq("Category was successfully saved.")
+    end
+  end
+
+
+
   it "test_index" do
     get :index
     assert_response :redirect, :action => 'index'
